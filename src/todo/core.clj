ff779493b5e377a12f5a96f402f51f5aca9a4272
Stujo <stujo@users.noreply.github.com>
2016-05-05 15:45:14 -0700

    Using apply require blank to build a command line app

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,7 +1,28 @@
 (ns todo.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :refer [blank?]]))
+
+(defn parse-string 
+    "Parse strings to number"
+    [x]
+    (if (string? x) 
+    	(if (blank? x) 
+    	    0 
+    	    (read-string x))
+    	x))
+
+(defn only-numbers
+	"extract numbers from inputs"
+	[inputs]
+	(filter number? (keep parse-string inputs))
+	)
+
+(defn adder 
+	"Add the numbers"
+	[& numbers] 
+	(apply + numbers))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Total of args = " (apply adder (only-numbers args))))
--- test/todo/core_test.clj
+++ test/todo/core_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer :all]
             [todo.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest adder-test
+  (testing "adder"
+    (is (= (adder 4 5) 9))))
+
+(deftest only-numbers-test
+  (testing "only-numbers"
+  	(is (= (only-numbers ["1" "c" "4"]) [1 4] ))
+  	(is (= (only-numbers ["1" "c" "4"]) [1 4] ))
+  	(is (= (only-numbers [1]) [1] ))
+  	(is (= (only-numbers ["x" "y" "z"]) [] ))
+	(is (= (only-numbers []) [] ))))
+
+(deftest parse-string-test
+  (testing "parse-string"
+  	(is (= (parse-string 1) 1 ))
+  	(is (= (parse-string "") 0 ))
+	(is (= (parse-string "1") 1 ))))

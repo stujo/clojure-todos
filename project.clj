879e20fa1289526646a2c7fa6faea0f618eaf264
Stujo <stujo@users.noreply.github.com>
2016-05-05 14:32:32 -0700

    Update project with todo infor

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject todo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Todo App in Clojure - eventually"
+  :url "http://github.com/stujo/clojure-todos"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
